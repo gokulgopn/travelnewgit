e5799243ff4ed53fa57087af4e45f08cb291c65b
gokulgopn <pggokulgopan2001@gmail.com>
2023-05-08 11:25:16 +0530

    t added

---
+++ travelproject/travelproject/lambda.py
@@ -0,0 +1,2 @@
+x=lambda x:x*(x+89)**7
+print(x(3))
---
+++ travelproject/travelproject/travelapp/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from . models import Place
+from . models import Team
+# Create your views here.
+def demo(request):
+    obj=Place.objects.all()
+    var=Team.objects.all()
+    return  render(request, "index.html", {'result': obj, 'answer': var})
+
+
+# def addition(request):
+#     x=int(request.GET['num1'])
+#     y=int(request.GET['num2'])
+#     res=x+y
+#     return render(request,"about.html",{'result':res})
